0bafae2928d4d576e10409670d400f64f9dd1a73
LJD1024 <1758414041@qq.com>
2024-06-19 16:45:09 +0800

    实现容量查询

--- back/src/main/java/org/example/back/service/WmsContainerService.java
+++ back/src/main/java/org/example/back/service/WmsContainerService.java
@@ -3,6 +3,8 @@ package org.example.back.service;
 import org.example.back.domain.WmsContainer;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.List;
+
 /**
  * <p>
  *  服务类
@@ -12,5 +14,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2024-06-16
  */
 public interface WmsContainerService extends IService<WmsContainer> {
+    List<Integer> getDistinctContainerCount(String itemNo, String supplier);
 
 }
