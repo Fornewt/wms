be09cd8c002531591d5341fd762813897ba11286
Xloar <2539907177@qq.com>
2024-06-20 18:10:00 +0800

    供应商接口

--- back/src/main/java/org/example/back/controller/WmsSupplierController.java
+++ back/src/main/java/org/example/back/controller/WmsSupplierController.java
@@ -1,9 +1,16 @@
 package org.example.back.controller;
 
 
+import org.example.back.domain.WmsSupplier;
+import org.example.back.mapper.WmsSupplierMapper;
+import org.example.back.service.WmsSupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,6 +23,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/wmsSupplier")
 public class WmsSupplierController {
+    @Autowired
+    private WmsSupplierService wmsSupplierService;
+    @GetMapping("/getSupplierName") // 返回所有供应商名字
+    public List<String> getSupplierName() {
+        return wmsSupplierService.getSupplierName();
+    }
+
 
 }
 
